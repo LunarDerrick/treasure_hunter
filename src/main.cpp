f7f79c1a53cbed2aa217053363eadcfc2854abe5
LunarDerrick <derrickphb@gmail.com>
2025-07-31 17:50:30 +0800

    Library Test: initialize SDL function to validate SDL3 is working at compile and runtime. -temporarily disable gitignore for full transfer of device

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <SDL3/SDL.h>
 
-int main() {
-    std::cout << "Hello World";
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+    printf("SDL initialized successfully!\n");
+    SDL_Quit();
     return 0;
 }
